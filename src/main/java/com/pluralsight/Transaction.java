5679e27af18cbb0f9cf247f1cb4b3d7c5c74d1fc
lauramunozn0 <lauramunozlaw@gmail.com>
2025-10-17 00:41:45 -0400

    I edited the main toString to make it easier to read.

--- src/main/java/com/pluralsight/Transaction.java
+++ src/main/java/com/pluralsight/Transaction.java
@@ -46,14 +46,9 @@ public class Transaction {
         return amount;
     }
 
+
     @Override
     public String toString() {
-        return "Transaction{" +
-                "date=" + date +
-                ", time=" + time +
-                ", description='" + description + '\'' +
-                ", vendor='" + vendor + '\'' +
-                ", amount=" + amount +
-                '}';
-    }
-}
+        return String.format("%-12s %-10s %-30s %-20s %-10.2f",
+                date, time, description, vendor, amount);
+    }   }
